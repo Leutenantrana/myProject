a7ffd78e4a73b7b251ec9a1a5a6b559c766f859a
Leutenantrana <ranasagar974@gmail.com>
2024-02-05 17:07:13 +0530

    final edits

--- MERN-AI-ChatBot/backend/src/app.ts
+++ MERN-AI-ChatBot/backend/src/app.ts
@@ -10,6 +10,8 @@ const app = express();
 //middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
+
+app.use(express.static('dist'))
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
 //remove it in production
